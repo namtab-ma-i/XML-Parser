52ccbf71dfac5aef55a81159b65571a85f8d583d
Vera <chuiko.vera@gmail.com>
2017-01-13 19:20:07 +0200

    XML parser added, image can be saved to a specified directory

--- parser.php
+++ parser.php
@@ -1,22 +1,25 @@
 <?php
-
 $ini_array = parse_ini_file("config.ini");
 
-$url = '?u='.$ini_array['username'].'&p='.$ini_array['password'];
+$url = $ini_array['url'].'?u='.$ini_array['username'].'&p='.$ini_array['password'];
 
-$xml = simplexml_load_file($url);
+$xml = simplexml_load_file($url) or die("Error loading xml!");
 
+//TODO: Getting last article id must be here
+//Hardcoded last id (just for testing)
+$last_id = 33714;
 
+foreach ($xml as $article) {
+    $id = $article->articleId;
+    if($last_id >= $id)
+        break;
+    else {
+        //TODO: Save article to database
 
-?>
-<html>
-<head>
-    <title>Parser test</title>
-</head>
-<body>
-<?php
+        //Following code requires allow_url_fopen set to true
+        //TODO: Change image path
+        copy($article->image, '\uploads' . '/' . $id . '.jpg');
 
-
-?>
-</body>
-</html>
+        echo $id.' ';
+    }
+}
