a1340e9a3600da0280e679c0cfab8ca9ea51d1da
Vera <chuiko.vera@gmail.com>
2017-01-13 19:07:34 +0200

    Update parser.php

--- parser.php
+++ parser.php
@@ -2,7 +2,7 @@
 
 $ini_array = parse_ini_file("config.ini");
 
-$url = 'http://www.infomedia.co.at/wtsXmlmb.php?u='.$ini_array['username'].'&p='.$ini_array['password'];
+$url = '?u='.$ini_array['username'].'&p='.$ini_array['password'];
 
 $xml = simplexml_load_file($url);
 
